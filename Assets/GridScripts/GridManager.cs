db178e326ee482ad797b0a28f288b91e24ffb106
Marco Balletta <ballettamarco1@gmail.com>
2023-03-08 16:24:04 +0100

    aaa

--- Assets/GridScripts/GridManager.cs
+++ Assets/GridScripts/GridManager.cs
@@ -42,25 +42,4 @@ public class GridManager : MonoBehaviour
             y -= 1 * (gridData.cellSize.z + gridData.cellGap.z);
         }
     }
-
-    /*protected void CreateGrid()
-    {
-        Vector3 startPosition = new Vector3(gm.GameState.Columns * (grid.cellSize.x + grid.cellGap.x) / 2, 0, gm.GameState.Rows * (grid.cellSize.z + grid.cellGap.z) / 2);
-        float x = startPosition.x;
-        float y = startPosition.z;
-        for (int row = 0; row < gm.GameState.Rows; row++)
-        {
-            for (int column = gm.GameState.Columns - 1; column >= 0; column--)
-            {
-                var tile = Instantiate(tilePrefab, new Vector3(x, 0, y), Quaternion.identity);
-                tile.transform.SetParent(this.transform);
-                tile.transform.localScale = grid.cellSize;
-                x -= 1 * (grid.cellSize.x + grid.cellGap.x);
-                tile.Setup(gm, row, column, TileType.empty, false);
-                mapTiles[new Vector2Int(row, column)] = tile;
-            }
-            x = startPosition.x;
-            y -= 1 * (grid.cellSize.z + grid.cellGap.z);
-        }
-    }*/
 }
